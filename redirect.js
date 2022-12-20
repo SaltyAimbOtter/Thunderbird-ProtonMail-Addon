80e35916523723aba7e1190c783c0c454d1d62ba
saltyaimbotter <saltyaimbotter@gmail.com>
2022-12-20 23:08:18 +0100

    remove references to calender addon

--- redirect.js
+++ redirect.js
@@ -1,5 +1,5 @@
-browser.spacesToolbar.addButton('ProtonCalender', {
-    title: "Proton Calender",
+browser.spacesToolbar.addButton('ProtonMail', {
+    title: "Proton Mail",
     defaultIcons: "mail-logo-greyscale.svg",
     url: "https://mail.proton.me"
 });
